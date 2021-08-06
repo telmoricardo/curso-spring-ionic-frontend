4cb58bbc25ad2e39445121fe664776d26de5a6e4
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-08-05 22:53:50 -0300

    Extraindo o email do token e armazendando o em localStorage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { JwtHelperService } from "@auth0/angular-jwt";
+
 import { API_CONFIG } from "../config/api.config";
 import { CredenciaisDTO } from "../models/credenciais.dto";
 import { LocalUser } from "../models/local.user";
@@ -7,6 +9,8 @@ import { StorageService } from "./storage.service";
 
 @Injectable()
 export class AuthService{
+
+    jwtHelper : JwtHelperService = new JwtHelperService();
 
     constructor(public http: HttpClient, 
         public storage: StorageService){}
@@ -24,7 +28,8 @@ export class AuthService{
     successfulLogin(authorizationValue : string){
         let tok = authorizationValue.substring(7);
         let user : LocalUser = {
-            token: tok
+            token: tok,
+            email: this.jwtHelper.decodeToken(tok).sub
         };
 
         this.storage.setLocalUser(user);
