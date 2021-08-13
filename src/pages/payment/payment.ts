5e9e66a50da9ef6e46fffd3c7a52eb867ca57a10
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-08-13 18:03:49 -0300

    Informando pedido registrado ao usuario

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -34,8 +34,7 @@ export class PaymentPage {
 
   nextPage(){
     this.pedido.pagamento = this.formGroup.value;
-    this.navCtrl.setRoot('OrderConfirmationPage', {pedido: this.pedido});
-    
+    this.navCtrl.setRoot('OrderConfirmationPage', {pedido: this.pedido});    
   }
 
 }
