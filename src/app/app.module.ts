24380f8bdc44df6c30bb1d4ffc8e8d33e25cbdbb
Telmo Ricardo <telmoricardorosa@gmail.com>
2021-08-13 17:07:12 -0300

    Salvando o pedido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ClienteService } from '../services/domain/cliente.service';
 import { AuthInterceptorProvider } from '../interceptors/auth.interceptor';
 import { ProdutoService } from '../services/domain/produto.service';
 import { CartService } from '../services/domain/cart.service';
+import { PedidoService } from '../services/domain/pedido.service';
 
 @NgModule({
   declarations: [
@@ -41,7 +42,8 @@ import { CartService } from '../services/domain/cart.service';
     StorageService,
     ClienteService,
     ProdutoService,
-    CartService
+    CartService,
+    PedidoService
   ]
 })
 export class AppModule {}
